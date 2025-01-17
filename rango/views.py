0859368411ed91ac7a9b4061f5803dcdd7e2d77b
Alex Carr <2730563c@student.gla.ac.uk>
2025-01-17 16:18:15 +0000

    added static and media images

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!. Head to the <a href='/rango/about/'>About</a> page.")
+    # Construct a dict to pass to the template as its context
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    # Return a rendered response to send to the cient
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. Return to the <a href='/rango/'>Index</a> page.")
+    return render(request, 'rango/about.html')
