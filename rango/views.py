c687d69749b36f10c4403ca6496558169f83ce41
Alex Carr <2730563c@student.gla.ac.uk>
2025-01-18 17:47:48 +0000

    added categories and pages to index. added category page

--- rango/views.py
+++ rango/views.py
@@ -1,17 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from rango.models import Category
+from rango.models import Category, Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
 
     # Construct a dict to pass to the template as its context
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
 
     # Return a rendered response to send to the cient
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        # Attempt to get a category with the slug
+        # Raises DoesNotExist
+        category = Category.objects.get(slug=category_name_slug)
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+
+        # Add cat to context to verify existence in template
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
