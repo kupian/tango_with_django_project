c1eed07824de81740e54032ed31a787e7eaac8a9
Alex Carr <2730563c@student.gla.ac.uk>
2025-01-20 21:37:51 +0000

    added page to add new category

--- rango/models.py
+++ rango/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 
+MAX_NAME_LENGTH = 128
+
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    name = models.CharField(max_length=MAX_NAME_LENGTH, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
